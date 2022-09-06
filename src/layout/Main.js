f8af3d943a54b2f011dd5acbb7eab155876930b0
酷酷的宝 <1727283040@qq.com>
2022-09-06 18:03:18 +0800

    update

--- src/layout/Main.js
+++ src/layout/Main.js
@@ -3,14 +3,11 @@ import {
   MenuFoldOutlined,
   MenuUnfoldOutlined,
 } from '@ant-design/icons';
-import { Route } from 'react-router-dom'
-import Test1 from '@/components/test1';
-import Test2 from '@/components/test2';
-import Test3 from '@/components/test3';
 import { routes } from '@/router';
 import { Layout } from 'antd'
 import { connect } from 'react-redux'
 import { toggleCollapse } from '@/redux/sider'
+import Breadcrumb from '@/components/Breadcrumb';
 const { Header, Content } = Layout
 
 class Main extends Component {
@@ -20,20 +17,22 @@ class Main extends Component {
     return (
       <Layout>
         <Header>
-          {React.createElement(collapse ? MenuUnfoldOutlined : MenuFoldOutlined, {
-            className: 'trigger',
-            style:{
-              color:'#000'
-            },  
-            onClick: () => toggleCollapse(!collapse),
-          })}
+          <div className='df aic'>
+            {React.createElement(collapse ? MenuUnfoldOutlined : MenuFoldOutlined, {
+              className: 'trigger',
+              style:{
+                color:'#000',
+                marginRight:10
+              },  
+              onClick: () => toggleCollapse(!collapse),
+            })}
+            <Breadcrumb />
+          </div>
+          <div>
+            login
+          </div>
         </Header>
-        <Content>
-            {/* <Route path="/test1" component={Test1}></Route>
-            <Route path="/test2" component={Test2}></Route>
-            <Route path="/test3" component={Test3}></Route> */}
-            {routes}
-        </Content>
+        <Content>{routes}</Content>
       </Layout>
     );
   }
